5542a5dd1ea9ca84720d5a15d3e2a6fa90be2f92
Creepyfie <fynjybjyb@mail.ru>
2024-01-31 22:48:40 +0300

    Update docker-compose.yml

--- service/src/main/java/com/protvino/splitwise/adapter/GroupDAO.java
+++ service/src/main/java/com/protvino/splitwise/adapter/GroupDAO.java
@@ -4,12 +4,9 @@ import com.protvino.splitwise.domain.request.EditGroupRequest;
 import com.protvino.splitwise.domain.value.Group;
 
 public interface GroupDAO {
-
-    long create(EditGroupRequest editGroupRequest);
-
+    Long create(EditGroupRequest editGroupRequest);
     void update(Long id, EditGroupRequest editGroupRequest);
-
     void delete(Long id);
-
     Group findById(Long id);
+
 }
