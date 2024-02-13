d7d6d7bead038726731f649e0dd46f13525d8f01
Creepyfie <fynjybjyb@mail.ru>
2024-02-13 20:13:18 +0300

    Added InMemory Dao for Debts

--- service/src/test/java/com/protvino/splitwise/_inmem/InMemoryDebtInExpense.java
+++ service/src/test/java/com/protvino/splitwise/_inmem/InMemoryDebtInExpense.java
@@ -5,6 +5,7 @@ import com.protvino.splitwise.domain.request.EditDebtInExpenseRequest;
 import com.protvino.splitwise.domain.value.DebtInExpense;
 import org.testcontainers.shaded.com.google.common.hash.HashCode;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -16,23 +17,38 @@ public class InMemoryDebtInExpense implements DebtInExpenseDao {
 
     @Override
     public void create(EditDebtInExpenseRequest request) {
+
         long id = 1l;
+
         debts.put(id,new DebtInExpense(request.getExpenseId(), request.getFromParticipantId()
         , request.getToParticipantId(), request.getAmount()));
     }
 
     @Override
     public void update(EditDebtInExpenseRequest request) {
-        if(debts.);
+
+        if(debts.containsKey(1l)){
+            debts.put(1l,new DebtInExpense(request.getExpenseId(), request.getFromParticipantId()
+                    , request.getToParticipantId(), request.getAmount()));
+        }
     }
 
     @Override
     public void delete(EditDebtInExpenseRequest request) {
 
+        debts.remove(1l);
     }
 
     @Override
     public List<DebtInExpense> findByExpenseId(Long expenseId) {
-        return null;
+
+        List<DebtInExpense> result = new ArrayList<>();
+
+        for (Map.Entry<Long,DebtInExpense> debt: debts.entrySet()
+             ) {
+            if (debt.getValue().getExpenseId() == expenseId)
+                result.add(debt.getValue());
+        }
+        return result;
     }
 }
